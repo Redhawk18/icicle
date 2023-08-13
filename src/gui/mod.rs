15f1d18558b0adcf7179d75a35192e7245bfdf2e
Redhawk18 <redhawk76767676@gmail.com>
2023-08-13 01:49:49 +0000

    added selection list

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,5 +1,6 @@
 mod widgets;
 use widgets::number_input::number_input;
+use widgets::selection_list::{Time, options, selection_list};
 use iced::widget::{row, text};
 use iced::{Element, Sandbox};
 use std::time::Duration;
@@ -8,10 +9,13 @@ pub struct Icicle {
     interval: Duration,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub enum Message {
     //number input
     NumberInput(u64),
+
+    //selection list
+    SelectionList((usize, String)),
 }
 
 impl Sandbox for Icicle {
@@ -32,14 +36,16 @@ impl Sandbox for Icicle {
             Message::NumberInput(interval) => {
                 self.interval = Duration::new(interval, 0);
             }
+            Message::SelectionList((i, text)) => println!("{i} {text}"),
+            
         }
     }
 
     fn view(&self) -> Element<Message> {
         row!(
             text("Press Interval"),
-            number_input(self.interval)
-            //selection_list()
+            number_input(self.interval),
+            selection_list(),
         )
         .into()
     }
---
+++ src/gui/widgets/selection_list.rs
@@ -0,0 +1,36 @@
+use crate::gui::Message;
+
+use iced::Length;
+use iced_aw::SelectionList;
+
+pub fn selection_list<'a>() -> SelectionList<'a, String, Message> {
+    let myvec = vec!["Hours", "Minutes", "Seconds", "Milliseconds", "Mircoseconds", "Nanoseconds"];
+    let myvec2 = vec![String::from("val1"), String::from("val2"), String::from("val3")];
+    SelectionList::new(myvec2, Message::SelectionList).height(Length::Fixed(50.0)).width(Length::Fixed(70.0))
+}
+
+#[derive(Debug, Clone, Copy, Default, Eq, Hash, PartialEq )]
+pub enum Time {
+    Hours,
+    Minutes,
+    #[default]
+    Seconds,
+    Milliseconds,
+    Mircoseconds,
+    Nanoseconds,
+}
+
+impl Time {
+    const ALL: [Time; 6] = [
+        Time::Hours,
+        Time::Minutes,
+        Time::Seconds,
+        Time::Milliseconds,
+        Time::Mircoseconds,
+        Time::Nanoseconds,
+    ];
+}
+
+pub fn options() -> Vec<&'static str> {
+    vec!["Hours", "Minutes", "Seconds", "Milliseconds", "Mircoseconds", "Nanoseconds"]
+}
