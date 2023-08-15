f4547b355b5e8bd25a4e305258b591606ed1eae8
Redhawk18 <redhawk76767676@gmail.com>
2023-08-15 01:50:38 +0000

    add every enum to options

--- src/gui/widgets/selection_list.rs
+++ src/gui/widgets/selection_list.rs
@@ -4,14 +4,22 @@ use iced::Length;
 use iced_aw::SelectionList;
 
 pub fn selection_list<'a>() -> SelectionList<'a, Time, Message> {
-    SelectionList::new(&[Time::Hours, Time::Minutes], Message::SelectionList)
-        .height(Length::Fixed(50.0))
-        .width(Length::Fixed(90.0))
+    SelectionList::new(
+        &[
+            Time::Minutes,
+            Time::Seconds,
+            Time::Milliseconds,
+            Time::Mircoseconds,
+            Time::Nanoseconds,
+        ],
+        Message::SelectionList,
+    )
+    .height(Length::Fixed(50.0))
+    .width(Length::Fixed(90.0))
 }
 
 #[derive(Debug, Clone, Copy, Default, Eq, Hash, PartialEq)]
 pub enum Time {
-    Hours,
     Minutes,
     #[default]
     Seconds,
@@ -26,7 +34,6 @@ impl std::fmt::Display for Time {
             f,
             "{}",
             match self {
-                Time::Hours => "Hours",
                 Time::Minutes => "Minutes",
                 Time::Seconds => "Seconds",
                 Time::Milliseconds => "Milliaseconds",
