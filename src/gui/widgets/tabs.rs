6a078716ce77227ff0568f8b77160d9c5da36f95
Redhawk18 <redhawk76767676@gmail.com>
2023-08-21 15:10:55 +0000

    refactor

--- src/gui/widgets/tabs.rs
+++ src/gui/widgets/tabs.rs
@@ -1,10 +1,10 @@
 use crate::gui::widgets::{
     number_input::number_input,
-    selection_list::{selection_key, selection_time},
+    selection_list::{selection_keys, selection_time},
 };
 use crate::gui::Message;
 
-use iced::widget::{column, row, text, text_input, Column};
+use iced::widget::{column, row, text, text_input, Row};
 use iced::Element;
 use iced_aw::{TabLabel, Tabs};
 
@@ -38,22 +38,23 @@ pub enum Input {
 
 fn body(input: Input, interval: u64) -> Element<'static, Message> {
     match input {
-        Input::Hold => row!(selection_key("Bind key"), selection_key("Input key"),).into(),
+        Input::Hold => selection_keys()
+        .into(),
         Input::Press => column!(
-            row!(selection_key("Bind key"), selection_key("Input key"),),
+            selection_keys(),
             text("Time"),
             number_input(interval),
             selection_time("Unit"),
         )
         .into(),
         Input::Sequence => column!(
-            row!(selection_key("Bind key"), selection_key("Input key"),),
+            selection_keys(),
             text("Time"),
             number_input(interval),
             selection_time("Unit"),
             column!(
                 text("sequence string"),
-                text_input("! would press shift+1", "")
+                text_input("! equals shift+1", "")
             )
         )
         .into(),
